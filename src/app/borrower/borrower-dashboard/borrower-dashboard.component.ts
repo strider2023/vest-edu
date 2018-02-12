9cf8b8800a18745032d669bc937533548c362617
saisree24 <saisree21@gmail.com>
2018-02-12 13:11:38 +0530

    updated borrower

--- src/app/borrower/borrower-dashboard/borrower-dashboard.component.ts
+++ src/app/borrower/borrower-dashboard/borrower-dashboard.component.ts
@@ -16,6 +16,22 @@ export class BorrowerDashboardComponent implements OnInit {
     { icon: 'settings', list: 'My Account', link: '' }
   ];
 
+  country = [
+    {value: 'United States - 0', countryname: 'United States'},
+    {value: 'New Zealand - 1', countryname: 'New Zealand'},
+    {value: 'Australia-2', countryname: 'Australia'},
+    {value: 'India-3', countryname: 'India'},
+    {value: 'Ireland-4', countryname: 'Ireland'}
+  ];
+
+  city = [
+    {value: 'hyderabad - 0', cityname: 'Hyderabad'},
+    {value: 'bengaluru - 1', cityname: 'Bengaluru'},
+    {value: 'khammam - 2', cityname: 'Khammam'},
+    {value: 'mumbai - 3', cityname: 'Mumbai'},
+    {value: 'pune - 4', cityname: 'Pune'}
+  ];
+
   constructor() {}
 
   ngOnInit() {}
